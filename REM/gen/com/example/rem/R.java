9f1ef48fe9da1202af0013a1b519de1f4e5ffe0b
Mark Wai <markwai@Marks-MacBook-Pro-2.local>
2014-01-29 17:32:50 -0500

    mark and kevin forever <3

--- REM/gen/com/example/rem/R.java
+++ REM/gen/com/example/rem/R.java
@@ -24,59 +24,122 @@ public final class R {
         public static final int alarm_grey=0x7f020000;
         public static final int alarm_white=0x7f020001;
         public static final int background=0x7f020002;
-        public static final int bpm_grey=0x7f020003;
-        public static final int bpm_white=0x7f020004;
-        public static final int change_time=0x7f020005;
-        public static final int gradient_background=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int movement_grey=0x7f020008;
-        public static final int movement_white=0x7f020009;
-        public static final int neutral=0x7f02000a;
-        public static final int sad=0x7f02000b;
-        public static final int sleeping=0x7f02000c;
-        public static final int smile=0x7f02000d;
-        public static final int start_sleep=0x7f02000e;
-        public static final int wakeup=0x7f02000f;
+        public static final int bg_main=0x7f020003;
+        public static final int bpm_grey=0x7f020004;
+        public static final int bpm_white=0x7f020005;
+        public static final int change_time=0x7f020006;
+        public static final int gradient_background=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int movement_grey=0x7f020009;
+        public static final int movement_white=0x7f02000a;
+        public static final int neutral=0x7f02000b;
+        public static final int sad=0x7f02000c;
+        public static final int sleeping=0x7f02000d;
+        public static final int smile=0x7f02000e;
+        public static final int start_sleep=0x7f02000f;
+        public static final int wakeup=0x7f020010;
+        public static final int wwe_logo=0x7f020011;
     }
     public static final class id {
-        public static final int action_settings=0x7f080010;
-        public static final int btnChangeTime=0x7f080005;
-        public static final int btnStart=0x7f080009;
-        public static final int button1=0x7f08000e;
-        public static final int heartrate=0x7f08000c;
+        public static final int btnChangeTime=0x7f08000c;
+        public static final int btnStart=0x7f080010;
+        public static final int button1=0x7f080016;
+        public static final int button_scan=0x7f08000b;
+        public static final int connect=0x7f080018;
+        public static final int emulatorView=0x7f080014;
+        public static final int heartrate=0x7f080013;
         public static final int imgLogo=0x7f080000;
-        public static final int lblTime=0x7f080007;
-        public static final int motion=0x7f08000b;
+        public static final int lblTime=0x7f08000e;
+        public static final int motion=0x7f080012;
         public static final int neutral=0x7f080003;
+        public static final int new_devices=0x7f08000a;
+        public static final int paired_devices=0x7f080008;
         public static final int sad=0x7f080004;
-        public static final int smartalarm=0x7f08000a;
+        public static final int smartalarm=0x7f080011;
         public static final int smile=0x7f080002;
         public static final int stopAlarm=0x7f080001;
-        public static final int test=0x7f08000f;
-        public static final int textView2=0x7f08000d;
-        public static final int timePicker1=0x7f080006;
-        public static final int tvTime=0x7f080008;
+        public static final int test=0x7f080017;
+        public static final int textView2=0x7f080015;
+        public static final int timePicker1=0x7f08000d;
+        public static final int title_left_text=0x7f080005;
+        public static final int title_new_devices=0x7f080009;
+        public static final int title_paired_devices=0x7f080007;
+        public static final int title_right_text=0x7f080006;
+        public static final int tvTime=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_splash=0x7f030000;
         public static final int alarm=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int main2=0x7f030003;
-        public static final int main3=0x7f030004;
+        public static final int custom_title=0x7f030002;
+        public static final int device_list=0x7f030003;
+        public static final int device_name=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int main2=0x7f030006;
+        public static final int main3=0x7f030007;
+        public static final int term_activity=0x7f030008;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int option_menu=0x7f070000;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int alarm_hello=0x7f050007;
+        public static final int alert_dialog_no=0x7f05000b;
+        public static final int alert_dialog_no_bt=0x7f050009;
+        public static final int alert_dialog_ok=0x7f05000d;
+        public static final int alert_dialog_turn_on_bt=0x7f05000e;
+        public static final int alert_dialog_warning_title=0x7f05000c;
+        public static final int alert_dialog_yes=0x7f05000a;
         public static final int app_name=0x7f050000;
         public static final int bpm=0x7f050004;
         public static final int btn_display=0x7f050006;
+        public static final int button_scan=0x7f050019;
+        /**  Options Menu 
+         */
+        public static final int connect=0x7f05001a;
+        public static final int default_value_color_preference=0x7f050030;
+        public static final int default_value_controlkey_preference=0x7f050031;
+        public static final int default_value_fontsize_preference=0x7f05002f;
+        /**  Don't localize these default values 
+         */
+        public static final int default_value_localecho_preference=0x7f05002e;
+        public static final int dialog_title_color_preference=0x7f050027;
+        public static final int dialog_title_controlkey_preference=0x7f05002b;
+        public static final int dialog_title_fontsize_preference=0x7f050024;
+        public static final int disconnect=0x7f05001b;
+        /**  Preference dialog 
+         */
+        public static final int general_preferences=0x7f05001e;
         public static final int hello_world=0x7f050002;
+        public static final int keyboard_preferences=0x7f050028;
         public static final int movement=0x7f050003;
+        public static final int none_found=0x7f050016;
+        public static final int none_paired=0x7f050015;
+        public static final int not_connected=0x7f05000f;
+        public static final int preferences=0x7f05001c;
         public static final int rem_alarm=0x7f050005;
+        /**   DeviceListActivity 
+         */
+        public static final int scanning=0x7f050013;
+        public static final int select_device=0x7f050014;
+        public static final int special_keys=0x7f05001d;
         public static final int stop_alarm=0x7f050008;
+        public static final int summary_color_preference=0x7f050026;
+        public static final int summary_controlkey_preference=0x7f05002a;
+        public static final int summary_fontsize_preference=0x7f050023;
+        public static final int summary_localecho_preference=0x7f050020;
+        public static final int text_preferences=0x7f050021;
+        public static final int title_color_preference=0x7f050025;
+        public static final int title_connected_to=0x7f050011;
+        public static final int title_connecting=0x7f050010;
+        public static final int title_controlkey_preference=0x7f050029;
+        public static final int title_document_key_press=0x7f05002c;
+        public static final int title_document_key_rest=0x7f05002d;
+        public static final int title_fontsize_preference=0x7f050022;
+        public static final int title_localhecho_preference=0x7f05001f;
+        public static final int title_not_connected=0x7f050012;
+        public static final int title_other_devices=0x7f050018;
+        public static final int title_paired_devices=0x7f050017;
     }
     public static final class style {
         /** 
@@ -106,4 +169,11 @@ public final class R {
          */
         public static final int AppTheme=0x7f060001;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a EmulatorView.
+         */
+        public static final int[] EmulatorView = {
+            
+        };
+    };
 }
