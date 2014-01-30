f9f0177fddbe0d727e6d91025407975eee1b39e7
Sisun Lee <sisun1030@gmail.com>
2014-01-30 11:43:57 -0500

    BlueTerm REM merge

---
+++ BlueTerm/src/es/pymasde/blueterm/AlarmReceiverActivity.java
@@ -0,0 +1,124 @@
+package es.pymasde.blueterm;
+
+import java.io.IOException;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class AlarmReceiverActivity extends Activity {
+    private MediaPlayer mMediaPlayer; 
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.alarm);
+
+        Button stopAlarm = (Button) findViewById(R.id.stopAlarm);
+        stopAlarm.setOnTouchListener(new OnTouchListener() {
+            public boolean onTouch(View arg0, MotionEvent arg1) {
+                mMediaPlayer.stop();
+                View b = findViewById(R.id.stopAlarm);
+                b.setVisibility(View.GONE);
+                
+                View smile = findViewById(R.id.smile);
+                smile.setVisibility(View.VISIBLE);
+                View neutral = findViewById(R.id.neutral);
+                neutral.setVisibility(View.VISIBLE);
+                View sad = findViewById(R.id.sad);
+                sad.setVisibility(View.VISIBLE);
+                //finish();
+                return false;
+            }
+        });
+        
+        //Clicked smiley face bitches:
+        Button smile = (Button) findViewById(R.id.smile);
+        smile.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+				System.out.println("User selected they were feeling happy");
+				Intent i = new Intent(AlarmReceiverActivity.this, MainActivity.class);
+                startActivity(i);
+			}
+		});
+        
+        //Clicked neutral face bitches:
+        Button neutral = (Button) findViewById(R.id.neutral);
+        neutral.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+				System.out.println("User selected they were feeling meh");
+				Intent i = new Intent(AlarmReceiverActivity.this, MainActivity.class);
+                startActivity(i);
+			}
+		});
+        
+        //Clicked sad face bitches:
+        Button sad = (Button) findViewById(R.id.sad);
+        sad.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+				System.out.println("User selected they were frowning");
+				Intent i = new Intent(AlarmReceiverActivity.this, MainActivity.class);
+                startActivity(i);
+			}
+		});
+        
+
+        playSound(this, getAlarmUri());
+    }
+    
+
+    private void playSound(Context context, Uri alert) {
+        mMediaPlayer = new MediaPlayer();
+        try {
+            mMediaPlayer.setDataSource(context, alert);
+            final AudioManager audioManager = (AudioManager) context
+                    .getSystemService(Context.AUDIO_SERVICE);
+            if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+                mMediaPlayer.prepare();
+                mMediaPlayer.start();
+            }
+        } catch (IOException e) {
+            System.out.println("OOPS");
+        }
+    }
+
+    
+    //Get an alarm sound. Try for an alarm. If none set, try notification, 
+    //Otherwise, ringtone.
+    private Uri getAlarmUri() {
+        Uri alert = RingtoneManager
+                .getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alert == null) {
+            alert = RingtoneManager
+                    .getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            if (alert == null) {
+                alert = RingtoneManager
+                        .getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+            }
+        }
+        return alert;
+    }
+    
+    
+}
---
+++ BlueTerm/src/es/pymasde/blueterm/MainActivity.java
@@ -0,0 +1,149 @@
+package es.pymasde.blueterm;
+
+import java.util.Calendar;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.content.Intent;
+import android.content.Context;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+
+ 
+public class MainActivity extends Activity {
+ 
+	private TextView tvDisplayTime;
+	private TimePicker timePicker1;
+	private ImageButton btnChangeTime, btnStartAlarm;
+	private CheckBox smartAlarm, heartRate, motion;
+	private int hour, minute, am_pm;
+	private String bpm_check, motion_check, smart_check;
+ 
+	static final int TIME_DIALOG_ID = 999;
+ 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+ 
+		setCurrentTimeOnView();
+		addListenerOnButton();
+ 
+	}
+ 
+	
+	// display current time
+	public void setCurrentTimeOnView() {
+ 
+		tvDisplayTime = (TextView) findViewById(R.id.tvTime);
+		timePicker1 = (TimePicker) findViewById(R.id.timePicker1);
+ 
+		final Calendar c = Calendar.getInstance();
+		hour = c.get(Calendar.HOUR_OF_DAY);
+		minute = c.get(Calendar.MINUTE);
+		am_pm = c.get(Calendar.AM_PM);
+ 
+		// set current time into textview
+		tvDisplayTime.setText(
+                    new StringBuilder().append(pad(hour))
+                                       .append(":").append(pad(minute)));
+ 
+		// set current time into timepicker
+		timePicker1.setCurrentHour(hour);
+		timePicker1.setCurrentMinute(minute);
+ 
+	}
+ 
+	
+	public void addListenerOnButton() {
+		
+		final Context context = this;
+		smartAlarm = (CheckBox) findViewById(R.id.smartalarm);
+		heartRate = (CheckBox) findViewById(R.id.heartrate);
+		motion = (CheckBox) findViewById(R.id.motion);
+				
+		Boolean a = smartAlarm.isChecked();
+		smart_check = a.toString();
+		Boolean b = heartRate.isChecked();
+		bpm_check = b.toString();
+		Boolean c = motion.isChecked();
+		motion_check = c.toString();
+ 
+		btnChangeTime = (ImageButton) findViewById(R.id.btnChangeTime);
+		btnChangeTime.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+				showDialog(TIME_DIALOG_ID);
+			}
+		});
+		
+		btnStartAlarm = (ImageButton) findViewById(R.id.btnStart);
+		btnStartAlarm.setOnClickListener(new OnClickListener() {
+ 
+			@Override
+			public void onClick(View v) {
+				System.out.println("Time selected and going to sleep");
+				
+				Intent intent = new Intent(context, BlueTerm.class);
+			    intent.putExtra("hour", hour);
+			    intent.putExtra("minute", minute);
+			    intent.putExtra("am_pm", am_pm);
+
+			    intent.putExtra("smart_alarm", smart_check);
+			    intent.putExtra("bpm", bpm_check);
+			    intent.putExtra("motion", motion_check);
+                startActivity(intent); 
+			}
+		});
+ 
+	}
+ 
+	
+	@Override
+	protected Dialog onCreateDialog(int id) {
+		switch (id) {
+		case TIME_DIALOG_ID:
+			// set time picker as current time
+			return new TimePickerDialog(this, 
+                                        timePickerListener, hour, minute,false);
+ 
+		}
+		return null;
+	}
+ 
+	
+	private TimePickerDialog.OnTimeSetListener timePickerListener = 
+            new TimePickerDialog.OnTimeSetListener() {
+		public void onTimeSet(TimePicker view, int selectedHour,
+				int selectedMinute) {
+			hour = selectedHour;
+			minute = selectedMinute;
+ 
+			// set current time into textview
+			tvDisplayTime.setText(new StringBuilder().append(pad(hour))
+					.append(":").append(pad(minute)));
+ 
+			// set current time into timepicker
+			timePicker1.setCurrentHour(hour);
+			timePicker1.setCurrentMinute(minute);
+ 
+		}
+	};
+ 
+	
+	private static String pad(int c) {
+		if (c >= 10)
+		   return String.valueOf(c);
+		else
+		   return "0" + String.valueOf(c);
+	}
+	
+	
+}
