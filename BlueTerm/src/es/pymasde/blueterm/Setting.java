8e73789018f55cecd3bb792963506f2b65042329
Sisun Lee <sisun1030@gmail.com>
2014-02-24 11:47:30 -0500

    setting and sleeploagp ages

---
+++ BlueTerm/src/es/pymasde/blueterm/Setting.java
@@ -0,0 +1,17 @@
+package es.pymasde.blueterm;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Setting extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test_page);
+	}
+
+
+}
