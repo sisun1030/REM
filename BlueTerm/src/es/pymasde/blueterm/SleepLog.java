35dbc1519350d2e59809b6d3b1f0bb20f78ae809
Sisun Lee <sisun1030@gmail.com>
2014-02-28 16:31:58 -0500

    first version of sqlite added

--- BlueTerm/src/es/pymasde/blueterm/SleepLog.java
+++ BlueTerm/src/es/pymasde/blueterm/SleepLog.java
@@ -1,17 +1,42 @@
 package es.pymasde.blueterm;
 
+import es.pymasde.blueterm.sqlite.DatabaseHandler;
+import es.pymasde.blueterm.data.Contact;
 
+import java.util.List;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 
 public class SleepLog extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.test_page);
-	}
-
-
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test_page);
+        
+        DatabaseHandler db = new DatabaseHandler(this);
+        
+        /**
+         * CRUD Operations
+         * */
+        // Inserting Contacts
+        Log.d("Insert: ", "Inserting ..");
+        db.addContact(new Contact("Ravi", "9100000000"));
+        db.addContact(new Contact("Srinivas", "9199999999"));
+        db.addContact(new Contact("Tommy", "9522222222"));
+        db.addContact(new Contact("Karthik", "9533333333"));
+ 
+        // Reading all contacts
+        Log.d("Reading: ", "Reading all contacts..");
+        List<Contact> contacts = db.getAllContacts();       
+ 
+        for (Contact cn : contacts) {
+            String log = "Id: "+cn.getID()+" ,Name: " + cn.getName() + " ,Phone: " + cn.getPhoneNumber();
+                // Writing Contacts to log
+            Log.d("Name: ", log);
+        
+        }
+    }
 }
